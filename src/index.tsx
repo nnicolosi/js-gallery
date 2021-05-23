e65df4f496f83f63f8631e70610ec5553f3bf9b6
Nick Nicolosi <nnicolosi@singlestoneconsulting.com>
2021-05-23 17:03:12 -0400

    Add esbuild and test transpilation in the browser

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,42 @@
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+    const ref = useRef<any>();
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm',
+        });
+    };
+
+    useEffect(() => {
+        startService();
+    }, []);
+
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015',
+        });
+
+        setCode(result.code);
+    };
+
     return <div>
-        <h1>Hi there!</h1>
+        <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+        <div>
+            <button onClick={onClick}>Submit</button>
+        </div>
+        <pre>{code}</pre>
     </div>
 }
 
