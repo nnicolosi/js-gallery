9e9b3fbd8330041124f3bc1848e5789d34b79878
Nick Nicolosi <nick.nicolosi@protonmail.com>
2021-05-24 21:09:47 -0400

    Refactor ESBuild plugins

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import { fetchPlugin } from './plugins/fetch-plugin';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
@@ -28,7 +29,10 @@ const App = () => {
             entryPoints: ['index.js'],
             bundle: true,
             write: false,
-            plugins: [unpkgPathPlugin()],
+            plugins: [
+                unpkgPathPlugin(),
+                fetchPlugin(input)
+            ],
             define: {
                 'process.env.NODE_ENV': '"production"',
                 global: 'window'
