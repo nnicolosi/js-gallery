2652918373da4128547b783ac38c464a7c7d4a33
Nick Nicolosi <nick.nicolosi@protonmail.com>
2021-05-27 22:43:32 -0400

    Add user convenience function to show arbitrary items in the preview window

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -7,6 +7,7 @@ import CodePreview from './code-preview';
 import { Cell } from '../state';
 import { useActions } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/use-typed-selector';
+import { useCumulativeCode } from '../hooks/use-cumulative-code';
 
 interface CodeCellProps {
     cell: Cell;
@@ -15,22 +16,23 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { updateCell, createBundle } = useActions();
     const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+    const cumulativeCode = useCumulativeCode(cell.id);
 
     useEffect(() => {
         if (!bundle) {
-            createBundle(cell.id, cell.content);
+            createBundle(cell.id, cumulativeCode);
             return;
         }
 
         const timer = setTimeout(async () => {
-            createBundle(cell.id, cell.content);
+            createBundle(cell.id, cumulativeCode);
         }, 750);
 
         return () => {
             clearTimeout(timer);
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [cell.id, cell.content, createBundle]);
+    }, [cumulativeCode, cell.id, createBundle]);
 
     return (
         <Resizable direction="vertical">
