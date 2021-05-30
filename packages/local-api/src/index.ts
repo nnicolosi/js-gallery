91fdc87e68278a2a91cfc899c0c3f9a431cce28b
Nick Nicolosi <nick.nicolosi@protonmail.com>
2021-05-30 17:39:41 -0400

    Set up for NPM publishing

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -15,7 +15,7 @@ export const serve = (port: number, filename: string, dir: string, useProxy: boo
             logLevel: 'silent'
         }));
     } else {
-        const packagePath = require.resolve('local-client/build/index.html');
+        const packagePath = require.resolve('@js-gallery/local-client/build/index.html');
         app.use(express.static(path.dirname(packagePath)));
     }
 
